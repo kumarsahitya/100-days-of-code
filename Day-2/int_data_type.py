b4f0fb89fa511f5b9fc46568b9cde6f185dbda57
Kumar Sahitya <kumar.sahitya@acquia.com>
2025-12-12 12:07:46 +0530

    add Python scripts for string sum, BMI calculator, integer and string data types, tip calculator, and weeks left calculator

---
+++ Day-2/int_data_type.py
@@ -0,0 +1,3 @@
+int_data_type = 123_345;
+
+print(int_data_type);
---
+++ Day-2/weeks_left.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+
+# Assuming average lifespan of 80 years
+average_lifespan = 90
+
+# Calculating remaining years
+years_left = average_lifespan - age
+
+# Converting years to weeks
+weeks_left = years_left * 52
+
+print(f"You have {years_left} weeks left.")
