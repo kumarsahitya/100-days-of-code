af08c27c591a3f07f6fcbff31e97c5c53d3a30bd
Kumar Sahitya <kumar.sahitya@acquia.com>
2025-12-30 11:11:23 +0530

    add new Python script for FizzBuzz implementation and a simple greeting function

---
+++ Day-6/basic_function.py
@@ -0,0 +1,17 @@
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
