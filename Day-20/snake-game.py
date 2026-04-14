689c32b0f2ee2e2c229ad19c934a8e110d11638a
Kumar Sahitya <kumar.sahitya@acquia.com>
2026-04-14 19:52:13 +0530

    add new Python scripts for snake game including snake movement and food generation

---
+++ Day-20/snake-game.py
@@ -0,0 +1,37 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.update()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        # snake.extend()
+
+
+
+
+
+
+screen.exitonclick()
