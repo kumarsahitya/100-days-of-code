9f3a234a73474103ea60b9df30f5f925221eb096
Kumar Sahitya <kumar.sahitya@acquia.com>
2026-04-15 15:11:37 +0530

    add game over functionality and extend snake feature in snake game

--- Day-20/snake.py
+++ Day-20/snake.py
@@ -14,11 +14,14 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -42,4 +45,8 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
