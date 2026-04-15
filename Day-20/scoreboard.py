b26f7604c455c23f5914f43b2e8be1ed8c478ed3
Kumar Sahitya <kumar.sahitya@acquia.com>
2026-04-15 14:57:13 +0530

    add scoreboard functionality to snake game for tracking scores

---
+++ Day-20/scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
--- Day-20/snake-game.py
+++ Day-20/snake-game.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,7 +29,9 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
         # snake.extend()
+
 
 
 
