d5863dbe179b3ae73662a39e1a3ef9d12746fb99
Kumar Sahitya <kumar.sahitya@acquia.com>
2026-04-28 14:56:25 +0530

    add initial implementation of snake game with movement, food, and scoreboard functionality

---
+++ Day-24/file-operations/file_operation.py
@@ -0,0 +1,9 @@
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew Text.")
+
+with open("new_file.txt", mode="w") as file:
+    file.write("New Text.")
